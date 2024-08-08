0d4e062edeac2c61106b9c3a898ac14e3fcb3185
Sam Sneed <163201376+sam-sneed@users.noreply.github.com>
2024-08-08 21:10:39 +0000

    Update tto.bat

--- tto.bat
+++ tto.bat
@@ -3,5 +3,5 @@ start p3d.exe
 REM echo Press any key when Panda3D is installed
 REM pause
 cd dev\win32
-start start-all-servers.bat
-start start-game.bat
+start start-all-servers-winemod.bat
+start start-game-winemod.bat
